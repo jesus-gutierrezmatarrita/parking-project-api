f70ee685da3a5fe86048dab5b80f03f5c2ef4138
Jesus Gutierrez Matarrita <jesus.gutierrezmatarrita@outlook.com>
2022-07-27 21:30:38 -0600

    Add Reservations CRUD

---
+++ Models/ProjectParkingJARSContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace parking_project_api.Models
+{
+    public partial class ProjectParkingJARSContext : DbContext
+    {
+        public ProjectParkingJARSContext()
+        {
+        }
+
+        public ProjectParkingJARSContext(DbContextOptions<ProjectParkingJARSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Reservation> Reservations { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=INFINITY-SKY;Initial Catalog=ProjectParkingJARS;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reservation>(entity =>
+            {
+                entity.ToTable("Reservation");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.CustomerId).HasColumnName("customer_id");
+
+                entity.Property(e => e.ParkingSpaceId).HasColumnName("parking_space_id");
+
+                entity.Property(e => e.ReservationDate)
+                    .HasColumnType("date")
+                    .HasColumnName("reservationDate");
+
+                entity.Property(e => e.TotalDue)
+                    .HasColumnName("totalDue")
+                    .HasDefaultValueSql("((0))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
